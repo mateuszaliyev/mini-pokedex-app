468abdf8ea8e7e7736d0bb61deee060626970185
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2025-07-20 20:40:35 +0200

    feat: add vercel preset

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   prerender: () => ["/"],
+  presets: [vercelPreset()],
   ssr: true,
 } satisfies Config;
